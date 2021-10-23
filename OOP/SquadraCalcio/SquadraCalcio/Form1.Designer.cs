e1c35ac18b745a664bae03c0b8af84b50df9a336
Andrea Vaira <a.vaira.1811@vallauri.edu>
2021-10-23 10:56:02 +0200

    Es squadraCalcio started

---
+++ OOP/SquadraCalcio/SquadraCalcio/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace SquadraCalcio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSquadra1 = new System.Windows.Forms.TextBox();
+            this.txtPunti3 = new System.Windows.Forms.TextBox();
+            this.txtPunti2 = new System.Windows.Forms.TextBox();
+            this.txtPunti1 = new System.Windows.Forms.TextBox();
+            this.txtSquadra3 = new System.Windows.Forms.TextBox();
+            this.txtSquadra2 = new System.Windows.Forms.TextBox();
+            this.btnCreaC = new System.Windows.Forms.Button();
+            this.btnApareggia = new System.Windows.Forms.Button();
+            this.btnBpareggia = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnVinceA = new System.Windows.Forms.Button();
+            this.btnVinceB = new System.Windows.Forms.Button();
+            this.btnVinceC = new System.Windows.Forms.Button();
+            this.btnCreaA = new System.Windows.Forms.Button();
+            this.btnCreaB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Atlante:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Virtus: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Macedonia: ";
+            // 
+            // txtSquadra1
+            // 
+            this.txtSquadra1.Location = new System.Drawing.Point(92, 6);
+            this.txtSquadra1.Name = "txtSquadra1";
+            this.txtSquadra1.ReadOnly = true;
+            this.txtSquadra1.Size = new System.Drawing.Size(80, 20);
+            this.txtSquadra1.TabIndex = 3;
+            // 
+            // txtPunti3
+            // 
+            this.txtPunti3.Location = new System.Drawing.Point(178, 62);
+            this.txtPunti3.Name = "txtPunti3";
+            this.txtPunti3.ReadOnly = true;
+            this.txtPunti3.Size = new System.Drawing.Size(80, 20);
+            this.txtPunti3.TabIndex = 4;
+            // 
+            // txtPunti2
+            // 
+            this.txtPunti2.Location = new System.Drawing.Point(178, 33);
+            this.txtPunti2.Name = "txtPunti2";
+            this.txtPunti2.ReadOnly = true;
+            this.txtPunti2.Size = new System.Drawing.Size(80, 20);
+            this.txtPunti2.TabIndex = 5;
+            // 
+            // txtPunti1
+            // 
+            this.txtPunti1.Location = new System.Drawing.Point(178, 6);
+            this.txtPunti1.Name = "txtPunti1";
+            this.txtPunti1.ReadOnly = true;
+            this.txtPunti1.Size = new System.Drawing.Size(80, 20);
+            this.txtPunti1.TabIndex = 6;
+            // 
+            // txtSquadra3
+            // 
+            this.txtSquadra3.Location = new System.Drawing.Point(92, 62);
+            this.txtSquadra3.Name = "txtSquadra3";
+            this.txtSquadra3.ReadOnly = true;
+            this.txtSquadra3.Size = new System.Drawing.Size(80, 20);
+            this.txtSquadra3.TabIndex = 7;
+            // 
+            // txtSquadra2
+            // 
+            this.txtSquadra2.Location = new System.Drawing.Point(92, 33);
+            this.txtSquadra2.Name = "txtSquadra2";
+            this.txtSquadra2.ReadOnly = true;
+            this.txtSquadra2.Size = new System.Drawing.Size(80, 20);
+            this.txtSquadra2.TabIndex = 8;
+            // 
+            // btnCreaC
+            // 
+            this.btnCreaC.Location = new System.Drawing.Point(178, 93);
+            this.btnCreaC.Name = "btnCreaC";
+            this.btnCreaC.Size = new System.Drawing.Size(80, 23);
+            this.btnCreaC.TabIndex = 16;
+            this.btnCreaC.Text = "Crea C";
+            this.btnCreaC.UseVisualStyleBackColor = true;
+            // 
+            // btnApareggia
+            // 
+            this.btnApareggia.Location = new System.Drawing.Point(6, 151);
+            this.btnApareggia.Name = "btnApareggia";
+            this.btnApareggia.Size = new System.Drawing.Size(80, 23);
+            this.btnApareggia.TabIndex = 17;
+            this.btnApareggia.Text = "A Pareggia";
+            this.btnApareggia.UseVisualStyleBackColor = true;
+            // 
+            // btnBpareggia
+            // 
+            this.btnBpareggia.Location = new System.Drawing.Point(92, 151);
+            this.btnBpareggia.Name = "btnBpareggia";
+            this.btnBpareggia.Size = new System.Drawing.Size(80, 23);
+            this.btnBpareggia.TabIndex = 18;
+            this.btnBpareggia.Text = "B Pareggia";
+            this.btnBpareggia.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(178, 151);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnVinceA
+            // 
+            this.btnVinceA.Location = new System.Drawing.Point(6, 122);
+            this.btnVinceA.Name = "btnVinceA";
+            this.btnVinceA.Size = new System.Drawing.Size(80, 23);
+            this.btnVinceA.TabIndex = 20;
+            this.btnVinceA.Text = "Vince A";
+            this.btnVinceA.UseVisualStyleBackColor = true;
+            // 
+            // btnVinceB
+            // 
+            this.btnVinceB.Location = new System.Drawing.Point(92, 122);
+            this.btnVinceB.Name = "btnVinceB";
+            this.btnVinceB.Size = new System.Drawing.Size(80, 23);
+            this.btnVinceB.TabIndex = 21;
+            this.btnVinceB.Text = "Vince B";
+            this.btnVinceB.UseVisualStyleBackColor = true;
+            // 
+            // btnVinceC
+            // 
+            this.btnVinceC.Location = new System.Drawing.Point(178, 122);
+            this.btnVinceC.Name = "btnVinceC";
+            this.btnVinceC.Size = new System.Drawing.Size(80, 23);
+            this.btnVinceC.TabIndex = 22;
+            this.btnVinceC.Text = "Vince C";
+            this.btnVinceC.UseVisualStyleBackColor = true;
+            // 
+            // btnCreaA
+            // 
+            this.btnCreaA.Location = new System.Drawing.Point(6, 93);
+            this.btnCreaA.Name = "btnCreaA";
+            this.btnCreaA.Size = new System.Drawing.Size(80, 23);
+            this.btnCreaA.TabIndex = 23;
+            this.btnCreaA.Text = "Crea A";
+            this.btnCreaA.UseVisualStyleBackColor = true;
+            // 
+            // btnCreaB
+            // 
+            this.btnCreaB.Location = new System.Drawing.Point(92, 93);
+            this.btnCreaB.Name = "btnCreaB";
+            this.btnCreaB.Size = new System.Drawing.Size(80, 23);
+            this.btnCreaB.TabIndex = 24;
+            this.btnCreaB.Text = "Crea B";
+            this.btnCreaB.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 450);
+            this.Controls.Add(this.btnCreaB);
+            this.Controls.Add(this.btnCreaA);
+            this.Controls.Add(this.btnVinceC);
+            this.Controls.Add(this.btnVinceB);
+            this.Controls.Add(this.btnVinceA);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnBpareggia);
+            this.Controls.Add(this.btnApareggia);
+            this.Controls.Add(this.btnCreaC);
+            this.Controls.Add(this.txtSquadra2);
+            this.Controls.Add(this.txtSquadra3);
+            this.Controls.Add(this.txtPunti1);
+            this.Controls.Add(this.txtPunti2);
+            this.Controls.Add(this.txtPunti3);
+            this.Controls.Add(this.txtSquadra1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSquadra1;
+        private System.Windows.Forms.TextBox txtPunti3;
+        private System.Windows.Forms.TextBox txtPunti2;
+        private System.Windows.Forms.TextBox txtPunti1;
+        private System.Windows.Forms.TextBox txtSquadra3;
+        private System.Windows.Forms.TextBox txtSquadra2;
+        private System.Windows.Forms.Button btnCreaC;
+        private System.Windows.Forms.Button btnApareggia;
+        private System.Windows.Forms.Button btnBpareggia;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnVinceA;
+        private System.Windows.Forms.Button btnVinceB;
+        private System.Windows.Forms.Button btnVinceC;
+        private System.Windows.Forms.Button btnCreaA;
+        private System.Windows.Forms.Button btnCreaB;
+    }
+}
+
