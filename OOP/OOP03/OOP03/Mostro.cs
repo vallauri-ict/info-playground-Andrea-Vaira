b5e722d59209401f889763d4de8fbddd5c56683e
Stud. Andrea Vaira <a.vaira.1811@vallauri.net>
2021-10-30 10:56:55 +0200

    Es OOP03 started

---
+++ OOP/OOP03/OOP03/Mostro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP03
+{
+    class Mostro
+    {
+        public string nome;
+        public double punti;
+        public Mostro()
+        {
+        }
+        public Mostro(string n)
+        {
+            nome = n;
+            punti = 100;
+        }
+        public double Valore()
+        { // punti del Mostro
+            return punti;
+        }
+    }
+}
