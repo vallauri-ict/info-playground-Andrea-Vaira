2dff4bf6a82e2e8236c62d04f6302b382def036a
Stud. Andrea Vaira <a.vaira.1811@vallauri.net>
2021-12-04 11:01:06 +0100

    Es Regex

---
+++ OOP/RegEx01/RegEx01/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace RegEx01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStr = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnMatch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inserisci la stringa da controllare";
+            // 
+            // txtStr
+            // 
+            this.txtStr.Location = new System.Drawing.Point(175, 6);
+            this.txtStr.Name = "txtStr";
+            this.txtStr.Size = new System.Drawing.Size(167, 20);
+            this.txtStr.TabIndex = 1;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(120, 44);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(89, 31);
+            this.btnTest.TabIndex = 2;
+            this.btnTest.Text = "Verifica";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(372, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Uso di match nella stringa \"One car red car blue car\" ";
+            // 
+            // btnMatch
+            // 
+            this.btnMatch.Location = new System.Drawing.Point(452, 44);
+            this.btnMatch.Name = "btnMatch";
+            this.btnMatch.Size = new System.Drawing.Size(89, 31);
+            this.btnMatch.TabIndex = 4;
+            this.btnMatch.Text = "Test stringa";
+            this.btnMatch.UseVisualStyleBackColor = true;
+            this.btnMatch.Click += new System.EventHandler(this.btnMatch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 106);
+            this.Controls.Add(this.btnMatch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtStr);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStr;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnMatch;
+    }
+}
+
---
+++ OOP/RegEx01/RegEx01/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions; //Libreria da impementare
+
+namespace RegEx01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            //soluzione 1
+            Regex rgx = new Regex(@"^[a-zA-Z0-9]\d{2}[a-zA-Z0-9](-\d{3}){2}[A-Za-z0-9]$"); //metto @ perevitare di mettere tutte le volte // ma metto solo /
+            string str = txtStr.Text;
+            MessageBox.Show(str+"  "+rgx.IsMatch(str));
+
+            //soluzione 2
+            MessageBox.Show(str + "  " + Regex.IsMatch(str, @"^[a-zA-Z0-9]\d{2}[a-zA-Z0-9](-\d{3}){2}[A-Za-z0-9]$"));
+        }
+
+        private void btnMatch_Click(object sender, EventArgs e)
+        {
+            string str = "One car red car blue car";
+            string pattern = @"(\w+)\s+(car)";
+            int count = 0;
+
+            Regex reg = new Regex(pattern);
+            Match m = reg.Match(str);
+
+            while (m.Success)
+            {
+                MessageBox.Show("Trovato: gruppo "+(++count)+" Valore: "+m.Value+
+                                " alla posizione "+m.Index);
+                m=m.NextMatch();
+            }
+        }
+    }
+}
